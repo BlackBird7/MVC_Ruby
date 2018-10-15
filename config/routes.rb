760cbb7bb0ecc8dd19e3b6040c820df320c96a23
Leo Estrada <xxkillerxx7@gmail.com>
2018-10-15 17:32:53 +0000

    feat: Primera Version

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   #get 'hello/index'
   #get 'hello/saludo'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root:to=>'hello#index'
-  match 'saludo', :to=> 'hello#saludo', :via =>[:get]
+  root :to => 'hello#index'
+  match 'saludo', :to => 'hello#saludo', :via => [:get]
 end
